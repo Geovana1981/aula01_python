b4d5deba2b171684e489ab0dbaf22779d88b07de
Geovana1981 <gegepiedade@gmail.com>
2025-11-18 19:40:06 -0300

    Inicia o repositório da Aula1

---
+++ aula1/exemplo2.py
@@ -0,0 +1,65 @@
+# nome = input("Digite o nome: ")
+# print(f"Olá, {nome}!")
+
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))
+
+# resultado = n1 * n2
+
+# print(f"O resultado foi: {resultado}")
+
+#------------------------------------------------------------------------------------------------------------------------------#
+
+# nome_usuario = input("Informe o nome: ")
+
+# idade_usuario = int(input("Informe a idade: "))
+
+# if idade_usuario == 1:
+#     print(f"Você se chama {nome_usuario} e tem {idade_usuario} ano de idade.")
+
+# if idade_usuario > 1:
+#     print(f"Você se chama {nome_usuario} e tem {idade_usuario} anos de idade.")
+
+#------------------------------------------------------------------------------------------------------------------------------#
+
+# idade = int(input("Informe sua idade: "))
+
+# calculo = 2025 - idade
+
+# print(f"Você nasceu em {calculo}.")
+
+#------------------------------------------------------------------------------------------------------------------------------#
+
+# money = float(input("O valor do ingresso é de R$15,00. Informe sua quantia: R$ "))
+# ingresso = 15
+
+# total = money // ingresso
+
+# if total == 1:
+#     print(f"Você consegue comprar {total} ingresso.")
+
+# if total > 1:
+#     print(f"Você consegue comprar {total} ingressos.")
+
+#------------------------------------------------------------------------------------------------------------------------------#
+    
+# preco = float(input(f'Informe o preço:'))
+# qtd = int(input(f'Informe a quantidade: '))
+
+# total = preco * qtd
+# desconto = total * 0.1
+# valor_total = total - desconto
+
+# print(f'Valor total: {total}\nDesconto de 10%: {desconto}\nValor a pagar: {valor_total:.2f}')
+
+#------------------------------------------------------------------------------------------------------------------------------#
+
+# salario = float(input(f'Informe seu salário:'))
+# porcentagem= int(input(f'Informe o aumento que deseja: '))
+
+# acrescimo = salario * (porcentagem/100) 
+# total = salario + acrescimo
+
+# print(f'Salário inicial: {salario}\nAumento de: {porcentagem}%\nSalário final: {total:.2f}')
+
+#------------------------------------------------------------------------------------------------------------------------------#
